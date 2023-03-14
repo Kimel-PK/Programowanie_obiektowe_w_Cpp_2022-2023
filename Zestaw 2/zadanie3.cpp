ab6386fad04413094e0adc41cae7ee93ce8c8ee7
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2023-03-14 19:26:55 +0100

    Zestaw 2 - poprawka

--- Zestaw 2/zadanie3.cpp
+++ Zestaw 2/zadanie3.cpp
@@ -3,17 +3,17 @@
 
 using namespace std;
 
-template <typename Iterator>
-int accumulate(Iterator first, Iterator last) {
-    int sum = 0;
+template <typename Iterator, typename T>
+T accumulate(Iterator first, Iterator last) {
+    T sum = (T)0;
     for (; first != last; ++first)
-        sum += 1;
+        sum += *first;
     return sum;
 }
 
 int main() {
     vector<int> v = {1, 2, 3, 4, 5};
-    int result = accumulate(v.begin(), v.end());
+    int result = accumulate<vector<int>::iterator, int>(v.begin(), v.end());
     cout << "Sum: " << result << endl;
     return 0;
 }
